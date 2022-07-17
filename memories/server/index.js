d04a6cc9abefd0808337e918c2939365bc040c76
Alpha-creed <wynoral@gmail.com>
2022-07-17 23:38:06 +0000

    final commit

--- memories/server/index.js
+++ memories/server/index.js
@@ -3,10 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv'
 
 const app = express();
-
-// http://localhost:5000/posts
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -15,10 +15,9 @@ app.use(cors())
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = "mongodb+srv://AlphaCoder:fredwyno12@cluster0.fprefew.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, UseUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, UseUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
     .catch((error) => { console.log(error.message) })
 
