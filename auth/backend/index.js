e5d0f4c64744a68bc63b7769f244afd1b0b73312
Alpha-creed <wynoral@gmail.com>
2022-07-22 00:05:11 +0000

    final commit

--- auth/backend/index.js
+++ auth/backend/index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const routesUrls = require('./routes/routes')
+const cors = require('cors')
 
 dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("DATABASE connected"))
 
-app.listen(4000, () => console.log("sever is up and running"))
+app.use(express.json())
+app.use(cors())
+app.use('/app', routesUrls)
+app.listen(5000, () => console.log("sever is up and running"))
--- auth/backend/models/SignUpModels.js
+++ auth/backend/models/SignUpModels.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const signUpTemp = new mongoose.Schema({
     fullName: {
         type: String,
-        required: true,
+        // required: true,
     },
     usename: {
         type: String,
